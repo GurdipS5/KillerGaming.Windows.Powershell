0b3905e80cc6bbaedd3a4bffdf241817cf69b99c
Administrator <Administrator@GLOVSDEV01>
2022-04-03 00:22:18 +0100

    code

---
+++ Set-WindowsOs.ps1
@@ -0,0 +1,15 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.197
+	 Created on:   	03/04/2022 00:18
+	 Created by:   	Administrator
+	 Organization: 	
+	 Filename:     	Set-WindowsOs.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
